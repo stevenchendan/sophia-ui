2db2e8baa23944b5a544415ca68e1a9b6c7b405a
stevenchendan <qwe16165850@gmail.com>
2020-12-29 16:39:35 +1100

    add tsconfig and config webpack to support typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
+const path = require('path');
 module.exports = {
+  mode: 'development',
   entry: {
     index: './lib/index.tsx'
   },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'sophiaui',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
   module: {
     rules:[
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { 
+        test: /\.tsx?$/, 
+        loader: "ts-loader" 
+      }
     ]
   }
 }
