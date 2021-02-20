c2bb6deb4995bb09884d8d9f906cef703ad7d083
stevenchendan <qwe16165850@gmail.com>
2021-02-20 18:58:19 +1100

    import icons

--- lib/importIcons.tsx
+++ lib/importIcons.tsx
@@ -1,4 +1,4 @@
-let importAll = (requireContext:any) => requireContext.keys().forEach(requireContext)
+let importAll = (requireContext:any) => requireContext.keys().forEach(requireContext);
 try {
   importAll(require.context('./icons/', true, /\.svg$/))
 } catch (error) {
